1a112a9eb455d6f626b7651623e0cb1910d99a6f
sao91172 <75396894+sao91172@users.noreply.github.com>
2022-11-07 15:21:35 -0500

    Add files via upload

---
+++ Orji_Patel_assignment1/NodeType.java
@@ -0,0 +1,24 @@
+package Orji_Patel_assignment1;
+
+public class NodeType {
+
+    private ItemType info;
+    private NodeType next;
+
+    public ItemType getInfo() {
+        return info;
+    }
+
+    public NodeType getNext() {
+        return next;
+    }
+
+    public void setInfo(ItemType info) {
+        this.info = info;
+    }
+
+    public void setNext(NodeType next) {
+        this.next = next;
+    }
+
+}
